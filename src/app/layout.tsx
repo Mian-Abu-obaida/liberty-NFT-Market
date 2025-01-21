5beddae39fe29ddc59a962982d69f9ea99235ca9
Mian-Abu-obaida <mianabuobaida15@gmail.com>
2025-01-21 13:52:21 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from "react";
-import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
