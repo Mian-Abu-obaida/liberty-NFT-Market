d32cadd8ae0e7e81fae55664b72074841ac092a6
Mian-Abu-obaida <mianabuobaida15@gmail.com>
2025-01-21 13:45:19 +0500

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,14 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        screens: {
+          '2xl': '1320px',  // Max-width for screens 1400px+
+          'xl': '1140px',   // Max-width for screens 1200px+
+          'lg': '960px',    // Max-width for screens 992px+
+          'md': '720px',    // Max-width for screens 768px+
+          'sm': '540px',    // Max-width for screens 576px+
+        },
       },
     },
   },
